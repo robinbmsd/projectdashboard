5f28a038bc330af8f2709c276f829ba3c0656fc5
robinbravomavin <robinbmsd@gmail.com>
2026-04-15 10:24:28 +0700

    breadcrumb and navigation update

---
+++ navigation/breadcrumb.ts
@@ -0,0 +1,6 @@
+export const breadcrumbMap = [
+  {
+    match: /^\/dashboard\/[^/]+$/i,,
+    items: [{ label: 'Client Master', icon: 'i-lucide-building', to: '/home/dashboard' }]
+  },
+]
---
+++ navigation/navigation.ts
@@ -0,0 +1,7 @@
+import type { NavigationMenuItem } from '@nuxt/ui'
+
+export const navItems: NavigationMenuItem[] = [
+  { label: 'Dashboard', to: '/home/dashboard', icon: 'i-lucide-house' },
+  { label: 'Transaction History', to: '/history/transactions', icon: 'i-lucide-arrow-right-left' },
+  { label: 'System Log', to: '/systemlogs/logs', icon: 'i-lucide-server-cog'},
+]
