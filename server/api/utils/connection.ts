a1b07fd0add8a8612f141ce4799d5192a0829599
robinbravomavin <robinbmsd@gmail.com>
2026-04-15 13:53:49 +0700

    dartabase connection code update

--- server/api/utils/connection.ts
+++ server/api/utils/connection.ts
@@ -1,12 +1,14 @@
+import 'dotenv/config'
 import mysql from 'mysql2/promise'
-import { useRuntimeConfig } from '#imports'
-const config = useRuntimeConfig()
+
+console.log("CEK ENV HOST:", process.env.DB_HOST)
+console.log("CEK ENV USER:", process.env.DB_USER)
 
 const pool = mysql.createPool({
-    host: config.dbHost,
-    user: config.dbUser,
-    password: config.dbPassword,
-    database: config.dbName,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
