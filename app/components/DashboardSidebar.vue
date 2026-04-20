17002b90bd3281907bd56c7af26f7aade52659d4
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 18:38:06 +0700

    default.vue auto clodes code update

--- app/components/DashboardSidebar.vue
+++ app/components/DashboardSidebar.vue
@@ -4,7 +4,9 @@ import { useRoute } from 'vue-router'
 import { navItems } from '~~/navigation/navItems'
 
 const isSidebarOpen = useState('sidebarOpen', () => false)
+
 const route = useRoute()
+
 const activeNavItems = computed(() => {
     const currentPath = route.path
 
@@ -33,7 +35,7 @@ const logout = () => {
   <div
     class="w-60 shrink-0 border-r border-gray-200 dark:border-black p-4 h-screen flex flex-col bg-white dark:bg-[#121212] transition-transform duration-300 z-50 fixed inset-y-0 left-0 lg:static lg:translate-x-0" 
     :class="isSidebarOpen ? 'translate-x-0 shadow-2xl lg:shadow-none' : '-translate-x-full'"
-    >
+  >
   <div class="flex items-center justify-center mb-8 px-2">
     <img src="https://icons.veryicon.com/png/o/miscellaneous/broken-linear-icon/dashboard-81.png" alt="Logo Dashboard" class="h-12" />
   </div>
