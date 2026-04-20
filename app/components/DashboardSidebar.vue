9ec4bf180bbf43c958c68b720a980123023c84ba
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 14:30:23 +0700

    logout option update

--- app/components/DashboardSidebar.vue
+++ app/components/DashboardSidebar.vue
@@ -1,12 +1,16 @@
 <template>
-  <div>
-    <button @click="navigateTo('/login')">
+  <div class="w-60 h-screen bg-white border-r border-gray-200 p-4">
+    <div class="mb-8">
+      LOGO
+    </div>
+    <div class="mt-auto border-t pt-4">
+      <button @click="navigateTo('/login')">
       Logout
-    </button>"
+    </button>
+    </div>
   </div>
   </template>
 
 <script setup>
-// Fungsi dasar untuk pindah halaman di Nuxt
 const logout = () => navigateTo('/login')
 </script>
