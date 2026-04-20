952a6040205beba5413910e58600a388062c4d10
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 14:47:22 +0700

    merapikan navItems.ts

--- navigation/navitems.ts
+++ navigation/navitems.ts
@@ -1,7 +1,19 @@
 import type { NavigationMenuItem } from '@nuxt/ui'
 
 export const navItems: NavigationMenuItem[] = [
-  { label: 'Dashboard', to: '/home/dashboard', icon: 'i-lucide-house' },
-  { label: 'Transaction History', to: '/history/transactions', icon: 'i-lucide-arrow-right-left' },
-  { label: 'System Log', to: '/systemlogs/logs', icon: 'i-lucide-server-cog'},
+  { 
+    label: 'Dashboard',
+    to: '/home/dashboard',
+    icon: 'i-lucide-house' 
+  },
+  { 
+    label: 'Transaction History',
+    to: '/history/transactions',
+    icon: 'i-lucide-arrow-right-left'
+  },
+  {
+    label: 'System Log',
+    to: '/systemlogs/logs',
+    icon: 'i-lucide-server-cog'
+  },
 ]
