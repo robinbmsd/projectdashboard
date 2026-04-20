705dfe662936ed195aee86cc0473011599094958
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 15:17:05 +0700

    default.vue update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,4 +1,13 @@
 <template>
   <div class="flex h-screen w-full bg-white dar:bg-[#121212]">
-  </div>
+  
+    <div class="shrink-0 w-60 bg-gray-100 dark:bg-gray-900 text-center p-4">
+      Ini Sidebar
+    </div>
+
+    <div class="flex flex-col flex-1 min-w-0 overflow-hidden">
+      Ini Area Kanan
+    </div>
+
+</div>
 </template>
