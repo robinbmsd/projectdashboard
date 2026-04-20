a1250adadbfb03f6ee21d6cbf63abd5594a43300
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 15:27:28 +0700

    default.vue update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,6 +9,10 @@
       <div class="shrink-0 h-16 bg-gray-200 dark:bg-gray-800 text-center p-4">
         Ini Navbar
       </div>
+
+      <main class="flex-1 min-h-0 relative overflow-y-auto p-4">
+        <slot />
+      </main>
     </div>
 
 </div>
