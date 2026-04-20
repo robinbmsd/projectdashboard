f9e13d3610a4cb005c867f41ecef1cd924956f97
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 16:39:04 +0700

    default.vue code update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -9,9 +9,7 @@ const isSidebarOpen = useState('sidebarOpen', () => false)
 <template>
   <div class="flex h-screen w-full bg-white dar:bg-[#121212]">
   
-    <div class="shrink-0 w-60 bg-gray-100 dark:bg-gray-900 text-center p-4">
-      Ini Sidebar
-    </div>
+    <DashboardSidebar class="shrink-0"/>
 
     <div class="flex flex-col flex-1 min-w-0 overflow-hidden">
       <div class="shrink-0 h-16 bg-gray-200 dark:bg-gray-800 text-center p-4">
