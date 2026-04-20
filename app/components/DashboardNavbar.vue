09ce568ca37b948c08d199c97e3880a366816176
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 16:26:41 +0700

    DashboardNavbar.vue code update

---
+++ app/components/DashboardNavbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <UDashboardNavbar>
+    class="shrink-0 h-16 bg-white! dark:bg-[#121212]! border-b border-gray-200 dark:border-black"
+    :breadcrumb="false"
+    :toggle="false"
+  >
+  </UDashboardNavbar>
+
+</template>
