55bc9159a17edc46467a84ead69b44c6b09a4413
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 14:27:13 +0700

    create logout option on DashboardSidebar

--- app/components/DashboardSidebar.vue
+++ app/components/DashboardSidebar.vue
@@ -1,1 +1,12 @@
-]
+<template>
+  <div>
+    <button @click="navigateTo('/login')">
+      Logout
+    </button>"
+  </div>
+  </template>
+
+<script setup>
+// Fungsi dasar untuk pindah halaman di Nuxt
+const logout = () => navigateTo('/login')
+</script>
