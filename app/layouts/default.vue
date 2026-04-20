57ad063254a636466a1f33bb9eed0802d21ae030
robinbravomavin <robinbmsd@gmail.com>
2026-04-20 15:30:08 +0700

    default.vue script update

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+import DashboardSidebar from '~/components/DashboardSidebar.vue'
+import DashboardNavbar from '~/components/DashboardNavbar.vue'
+</script>
+
 <template>
   <div class="flex h-screen w-full bg-white dar:bg-[#121212]">
   
